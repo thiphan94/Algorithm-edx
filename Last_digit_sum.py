fc15dc300d7a6b69e747c906eee0ffb21df0f51f
unknown <thiphan94@gmail.com>
2020-07-02 01:08:12 +0200

    week 2

--- Last_digit_sum.py
+++ Last_digit_sum.py
@@ -3,19 +3,17 @@
 import sys
 
 
-def fibo_sum(n):
+def fibo(n):
+    n = (n + 2) % 60
     left = 0
     right = 1
-    sum = 1
-    if n == 0:
-        return left
-    elif n == 1:
-        return right
     for _ in range(n - 1):
-        left, right = right, left + right
-        sum += right
-    return sum
+        left, right = right % 10, (left + right) % 10
+    if right == 0:
+        return 9
+    else:
+        return right - 1
 
 
 n = int(input())
-print(fibo_sum(n) % 10)
+print(fibo(n))

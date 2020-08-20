a106952d61cb666b6cf6124fae5219fd2c0772fd
unknown <thiphan94@gmail.com>
2020-08-20 23:50:55 +0200

    week 5

--- edit_distance.py
+++ edit_distance.py
@@ -1,7 +1,24 @@
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    m = len(s)
+    n = len(t)
+    stock = [[[None] for i in range(n + 1)] for j in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                stock[i][j] = j
+            elif j == 0:
+                stock[i][j] = i
+            elif s[i - 1] == t[j - 1]:
+                stock[i][j] = stock[i - 1][j - 1]
+            else:
+                stock[i][j] = (
+                    min(stock[i][j - 1], stock[i - 1][j], stock[i - 1][j - 1]) + 1
+                )
+
+    return stock[m][n]
+
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
